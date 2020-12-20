51b6866806f296ac44372b4e657ec63eda4e7365
Yang Jia Fei <jfhyang@gmail.com>
2020-12-20 11:41:10 +0800

    remove wrong []

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 PORT=8080
 DATA_FOLDER=~/trilium-data
 TRILIUM_VERSION=0.45.6
-sudo docker run -t -i -p 127.0.0.1:$PORT:8080 -v $DATA_FOLDER:/root/trilium-data zadam/trilium:[$TRILIUM_VERSION]
+sudo docker run -t -i -p 127.0.0.1:$PORT:8080 -v $DATA_FOLDER:/root/trilium-data zadam/trilium:$TRILIUM_VERSION
