c5b41d11e55dfc88bd0916a301642942d95237df
Yang Jia Fei <jfyang@cn.ibm.com>
2020-12-21 10:44:29 +0800

    mirror fix

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 PORT=8080
 DATA_FOLDER=~/trilium-data
 TRILIUM_VERSION=0.45.6
-sudo docker run -t -i -p 127.0.0.1:$PORT:8080 -v $DATA_FOLDER:/root/trilium-data zadam/trilium:$TRILIUM_VERSION
+sudo docker run -t -i -p 0.0.0.0:$PORT:8080 -v $DATA_FOLDER:/root/trilium-data zadam/trilium:$TRILIUM_VERSION
