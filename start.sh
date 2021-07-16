f864c1509a619d99585bb12a28197a6b279d1ebb
Jia Fei Yang <jfhyang@gmail.com>
2021-07-16 17:58:22 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 PORT=8080
 DATA_FOLDER=~/trilium-data
-TRILIUM_VERSION=0.45.6
-sudo docker run -t -i -p 0.0.0.0:$PORT:8080 -v $DATA_FOLDER:/root/trilium-data zadam/trilium:$TRILIUM_VERSION
+TRILIUM_VERSION=0.47.4
+sudo docker run -d -p 0.0.0.0:$PORT:8080 -v $DATA_FOLDER:/root/trilium-data zadam/trilium:$TRILIUM_VERSION
